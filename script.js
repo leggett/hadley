c9cb63e533578d0c3ad2296e7265e0f30ce42c36
seattleowl <32573897+seattleowl@users.noreply.github.com>
2020-07-16 16:57:53 -0700

    Move page up on load

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
 let header = document.getElementById("header");
-
 window.onscroll = (e) => {
 	header.style.top = -Math.round(window.scrollY / 5) + "px";
 };
+
+let content = document.getElementById("content");
+setTimeout(() => {
+	content.style.marginTop = "90vh";
+}, 1000);
