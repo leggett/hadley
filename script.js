092490ed7071c2a67431fa25355b05b0f6a112d5
seattleowl <32573897+seattleowl@users.noreply.github.com>
2020-07-16 15:58:21 -0700

    Round paralax scroll to nearest number.

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 let header = document.getElementById("header");
 
 window.onscroll = (e) => {
-	header.style.top = -(window.scrollY / 5) + "px";
+	header.style.top = -Math.round(window.scrollY / 5) + "px";
 };
