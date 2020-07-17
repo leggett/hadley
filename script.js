a5b12ff08c7be92784485ba1a30eaf9c91e3e2c4
Michael Leggett <leggett@gmail.com>
2020-07-16 18:05:38 -0700

    Rest of fixes for mobile

--- script.js
+++ script.js
@@ -1,4 +1,9 @@
 let header = document.getElementById("header");
 window.onscroll = (e) => {
   header.style.top = -Math.round(window.scrollY / 5) + "px";
+  if (window.scrollY > 75) {
+    document.documentElement.classList.add("scrolled");
+  } else {
+    document.documentElement.classList.remove("scrolled");
+  }
 };
