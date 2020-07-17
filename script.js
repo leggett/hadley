d26a3d123d39d9f7bc8933d60d7e85d5eff94ffb
Michael Leggett <leggett@gmail.com>
2020-07-16 17:34:39 -0700

    Fixes for mobile

--- script.js
+++ script.js
@@ -1,9 +1,4 @@
 let header = document.getElementById("header");
 window.onscroll = (e) => {
-	header.style.top = -Math.round(window.scrollY / 5) + "px";
+  header.style.top = -Math.round(window.scrollY / 5) + "px";
 };
-
-let content = document.getElementById("content");
-setTimeout(() => {
-	content.style.marginTop = "90vh";
-}, 1000);
