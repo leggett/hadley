1f93611896de8c13d8c461c4be8385f982f9a0b8
Michael Leggett <leggett@gmail.com>
2020-07-17 00:04:42 -0700

    MOAR CHANGES

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 let header = document.getElementById("header");
 window.onscroll = (e) => {
   header.style.top = -Math.round(window.scrollY / 5) + "px";
-  if (window.scrollY > 75) {
+  if (window.scrollY > 100) {
     document.documentElement.classList.add("scrolled");
   } else {
     document.documentElement.classList.remove("scrolled");
