5fd1f036a049e5195888a516b3a528e2589d54cc
Michael Leggett <leggett@gmail.com>
2020-08-05 16:03:29 -0700

    Launching new site

--- script.js
+++ script.js
@@ -1,17 +1,23 @@
 let header = document.getElementById("header");
-window.onscroll = (e) => {
-	header.style.top = -Math.round(window.scrollY / 5) + "px";
-	if (window.scrollY > 100) {
-		document.documentElement.classList.add("scrolled");
-	} else {
-		document.documentElement.classList.remove("scrolled");
-	}
+window.onscroll = () => {
+  header.style.top = -Math.round(window.scrollY / 5) + "px";
+  if (window.scrollY > 100) {
+    document.documentElement.classList.add("scrolled");
+  } else {
+    document.documentElement.classList.remove("scrolled");
+  }
+};
+
+window.onload = () => {
+  if (window.scrollY > 100) {
+    document.documentElement.classList.add("scrolled");
+  }
 };
 
 const appHeight = () =>
-	document.documentElement.style.setProperty(
-		"--app-height",
-		`${window.innerHeight}px`
-	);
+  document.documentElement.style.setProperty(
+    "--app-height",
+    `${window.innerHeight}px`
+  );
 window.addEventListener("resize", appHeight);
 appHeight();
