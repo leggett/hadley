520aef0d31996f4545ed298b2986881ca6e014a6
seattleowl <32573897+seattleowl@users.noreply.github.com>
2021-01-04 07:11:24 -0800

    Fix #1 (Blind)

--- script.js
+++ script.js
@@ -1,23 +1,26 @@
 let header = document.getElementById("header");
+
+document.body.style = `--view-height:${window.innerHeight}`;
+
 window.onscroll = () => {
-  header.style.top = -Math.round(window.scrollY / 5) + "px";
-  if (window.scrollY > 100) {
-    document.documentElement.classList.add("scrolled");
-  } else {
-    document.documentElement.classList.remove("scrolled");
-  }
+	header.style.top = -Math.round(window.scrollY / 5) + "px";
+	if (window.scrollY > 100) {
+		document.documentElement.classList.add("scrolled");
+	} else {
+		document.documentElement.classList.remove("scrolled");
+	}
 };
 
 window.onload = () => {
-  if (window.scrollY > 100) {
-    document.documentElement.classList.add("scrolled");
-  }
+	if (window.scrollY > 100) {
+		document.documentElement.classList.add("scrolled");
+	}
 };
 
 const appHeight = () =>
-  document.documentElement.style.setProperty(
-    "--app-height",
-    `${window.innerHeight}px`
-  );
+	document.documentElement.style.setProperty(
+		"--app-height",
+		`${window.innerHeight}px`
+	);
 window.addEventListener("resize", appHeight);
 appHeight();
