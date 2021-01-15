145472489180c8959dfa42d0e5322a4f26b3de74
Michael Leggett <leggett@gmail.com>
2021-01-14 19:23:11 -0800

    Fix issue #1

--- script.js
+++ script.js
@@ -1,18 +1,18 @@
 let header = document.getElementById("header");
 
 window.onscroll = () => {
-  // header.style.top = -Math.round(window.scrollY / 5) + "px";
+  header.style.top = -Math.round(window.scrollY / 5) + "px";
   if (window.scrollY > 100) {
     document.documentElement.classList.add("scrolled");
   } else {
     document.documentElement.classList.remove("scrolled");
   }
 
-  if (window.scrollY > 1000) {
-    header.style.display = "none";
-  } else {
-    header.style.display = "grid";
-  }
+  // if (window.scrollY > 1000) {
+  //   header.style.display = "none";
+  // } else {
+  //   header.style.display = "grid";
+  // }
 };
 
 window.onload = () => {
@@ -23,15 +23,19 @@ window.onload = () => {
 
 const touchDevice = "ontouchstart" in document.documentElement;
 
-const appHeight = (firstTime = false) => {
-  if (firstTime || !touchDevice) {
-    document.documentElement.style.setProperty(
-      "--app-height",
-      `${window.innerHeight}px`
-    );
-    document.querySelector("#appHeight").innerText =
-      window.innerHeight + " / " + touchDevice + " / " + firstTime;
-  }
+const appHeight = (event) => {
+  if (touchDevice && event?.type === "resize") return;
+
+  document.documentElement.style.setProperty(
+    "--app-height",
+    `${window.innerHeight}px`
+  );
+  document.querySelector("#appHeight").innerText =
+    window.innerHeight +
+    " / " +
+    touchDevice +
+    " / " +
+    (event?.type === "resize");
 };
 
 window.addEventListener("resize", appHeight);
